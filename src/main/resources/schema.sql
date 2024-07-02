f24a56a5af9aebcaecaf08a36070582950a210f0
JShistory <behappy0916@naver.com>
2024-07-02 10:17:44 +0900

    초기데이터 test

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS subwayinfo (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    subway_Name VARCHAR(255),
-    subway_Line VARCHAR(255),
-    subway_Line_Id BIGINT
-)DEFAULT CHARSET=utf8;
+-- CREATE TABLE IF NOT EXISTS subway_info (
+--     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+--     subway_Name VARCHAR(255),
+--     subway_Line VARCHAR(255),
+--     subway_Line_Id BIGINT
+-- )DEFAULT CHARSET=utf8;
