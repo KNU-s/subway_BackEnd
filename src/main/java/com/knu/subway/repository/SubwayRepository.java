ea7f73d3774b0b0d317c1a6803b1864fe58fce7c
JShistory <behappy0916@naver.com>
2024-07-10 14:51:10 +0900

    fix: subwayRepository 복구

---
+++ src/main/java/com/knu/subway/repository/SubwayRepository.java
@@ -0,0 +1,10 @@
+package com.knu.subway.repository;
+
+import com.knu.subway.entity.Subway;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface SubwayRepository extends MongoRepository<Subway, String> {
+    List<Subway> findByStatnId(String statnId);
+}
