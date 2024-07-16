ea640f5d54a3b7bb6c39f225fd8e89699f154b98
JShistory <behappy0916@naver.com>
2024-07-16 16:11:44 +0900

    feat: 비동기 config 설정 추가

---
+++ src/main/java/com/knu/subway/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.knu.subway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);  // 코어 풀 크기 설정
+        executor.setMaxPoolSize(10);  // 최대 풀 크기 설정
+        executor.setQueueCapacity(100);  // 큐 용량 설정
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+}
+
