128be5d4db1c0358d8afa7a19f3c1a986509ae46
JShistory <behappy0916@naver.com>
2024-07-17 17:55:16 +0900

    feat: 쿠키처럼 동작하는 스케줄 생성 및 로직 변경

--- src/main/java/com/knu/subway/service/SubwayDataCollector.java
+++ src/main/java/com/knu/subway/service/SubwayDataCollector.java
@@ -5,12 +5,14 @@ import com.knu.subway.entity.Subway;
 import com.knu.subway.entity.dto.SubwayDTO;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,6 +26,8 @@ public class SubwayDataCollector {
     private List<StationInfo> stationInfoList;
     private Set<String> stationList;
     private List<Subway> subways;
+    private List<String> subwayCookie;
+    private static final Logger log = LoggerFactory.getLogger(ApiService.class);
 
 
     @PostConstruct
@@ -35,9 +39,15 @@ public class SubwayDataCollector {
 
         System.out.println("Initialized stationList: " + stationList); // stationList가 예상대로 초기화되었는지 확인
     }
-    @Scheduled(fixedRate = 10000)  // 10초마다 데이터 수집
+//    @Scheduled(fixedRate = 10000)  // 10초마다 데이터 수집
     public void collectData() {
         stationList.forEach(this::collectDataByLineAsync); // 비동기 메서드 호출
+    }
+
+//    @Scheduled(fixedRate = 1800000)  // 30분마다 쿠키 초기화
+    public void subwayCookie() {
+        log.info("delete Subway Cookie {} : ",subwayCookie);
+        subwayCookie = new ArrayList<>();
     }
 
     @Async("taskExecutor")
@@ -55,10 +65,25 @@ public class SubwayDataCollector {
         subways = new ArrayList<>();
         for(SubwayDTO data : subwayDTO){
             Subway subway = data.toEntity();
-//            if(subway.getDstStation())
+            //도착지 역, 현재 역
+            String dstStation = subway.getDstStation();
+            String curStation = subway.getStatnId();
+            String trainStatus = subway.getTrainStatus();
             List<Subway> trainId = subwayService.findByTrainId(subway.getTrainId());
             if(trainId.isEmpty()) {
-                subways.add(subway);
+                if(!subwayCookie.contains(subway.getTrainId())){
+                    subways.add(subway);
+                }
+            } else {
+                // dstStation과 curStation이 null이 아닌지 확인
+                if (dstStation != null && curStation != null && trainStatus != null) {
+                    if(dstStation.contains(curStation) && Objects.equals(trainStatus, "도착")){
+                        subwayService.delete(trainId.get(0));
+                        subwayCookie.add(trainId.get(0).getTrainId());
+                        continue;
+                    }
+                }
+                subways.add(subwayService.update(trainId.get(0), data));
             }
         }
         subwayService.saveAll(subways);
