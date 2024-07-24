78623b0164abdd83623c7e8e71ceb9d2090aa726
JShistory <behappy0916@naver.com>
2024-07-24 14:40:43 +0900

    style: 주석 추가 및 log변경

--- src/main/java/com/knu/subway/service/SubwayDataCollector.java
+++ src/main/java/com/knu/subway/service/SubwayDataCollector.java
@@ -5,8 +5,7 @@ import com.knu.subway.entity.Subway;
 import com.knu.subway.entity.dto.SubwayDTO;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class SubwayDataCollector {
@@ -25,10 +24,7 @@ public class SubwayDataCollector {
     private final ApiService apiService;
     private List<StationInfo> stationInfoList;
     private Set<String> stationList;
-    private List<Subway> subways;
-    private List<String> subwayCookie;
-    private static final Logger log = LoggerFactory.getLogger(ApiService.class);
-
+    private List<String> subwayCookie = new ArrayList<>();
 
     @PostConstruct
     private void init() {
@@ -47,12 +43,11 @@ public class SubwayDataCollector {
     @Scheduled(fixedRate = 1800000)  // 30분마다 쿠키 초기화
     public void subwayCookie() {
         log.info("delete Subway Cookie {} : ",subwayCookie);
-        subwayCookie = new ArrayList<>();
+        subwayCookie.clear();
     }
 
     @Async("taskExecutor")
     public void collectDataByLineAsync(String line) {
-//        System.out.println("Collecting data asynchronously for line: " + line); // line 값이 예상대로 출력되는지 확인
         stationInfoList.stream()
                 .filter(station -> station.getStationLine().equals(line))
                 .forEach(station -> {
@@ -68,19 +63,21 @@ public class SubwayDataCollector {
     }
     private void saveSubwayInfo(List<SubwayDTO> subwayDTOList) {
         List<Subway> subwaysToSave = new ArrayList<>();
-
         for (SubwayDTO data : subwayDTOList) {
             Subway subway = data.toEntity();
             String dstStation = subway.getDstStation();
             String curStation = subway.getStatnId();
             String trainStatus = subway.getTrainStatus();
             List<Subway> existingTrains = subwayService.findByTrainId(subway.getTrainId());
-
+            //열차번호가 존재하지 않는다면
             if (existingTrains.isEmpty()) {
+                //최근에 삭제된 열차가 아니라면
                 if (!subwayCookie.contains(subway.getTrainId())) {
                     subwaysToSave.add(subway);
                 }
+            //열자번호가 존재한다면
             } else {
+                //현재 열차가 도착 상태이고, 마지막 역 까지 도달했다면
                 if (shouldDeleteExistingTrain(dstStation, curStation, trainStatus)) {
                     Subway existingTrain = existingTrains.get(0);
                     subwayService.delete(existingTrain);
