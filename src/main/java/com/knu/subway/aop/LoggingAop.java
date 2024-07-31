ad9379e2d688f4cfd86facaa849b16a46c5850b9
JShistory <behappy0916@naver.com>
2024-07-31 12:48:45 +0900

    fix: AOP 에러 수정

--- src/main/java/com/knu/subway/aop/LoggingAop.java
+++ src/main/java/com/knu/subway/aop/LoggingAop.java
@@ -1,7 +1,6 @@
 package com.knu.subway.aop;
 
 import com.knu.subway.entity.Subway;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
@@ -12,12 +11,14 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.List;
+
 @Aspect
 @Component
 @Slf4j
 public class LoggingAop {
 
-    @Before("execution(* com.knu.subway.webSocket.WebSocketHandler.handleTextMessage(..)) && args(session, message)")
+    @Before(value = "execution(* com.knu.subway.webSocket.WebSocketHandler.handleTextMessage(..)) && args(session, message)", argNames = "session,message")
     public void logBeforeHandleTextMessage(WebSocketSession session, TextMessage message) {
         log.info("Received Message: {}", message.getPayload());
     }
@@ -28,20 +29,20 @@ public class LoggingAop {
         log.info("Session ID: {}", session.getId());
     }
 
-    @AfterReturning("execution(* com.knu.subway.webSocket.WebSocketHandler.afterConnectionClosed(..)) && args(session, status)")
+    @AfterReturning(value = "execution(* com.knu.subway.webSocket.WebSocketHandler.afterConnectionClosed(..)) && args(session, status)", argNames = "session,status")
     public void logAfterConnectionClosed(WebSocketSession session, CloseStatus status) {
         log.info("WebSocket Disconnected");
         log.info("Session ID: {}", session.getId());
     }
 
-    @AfterReturning(pointcut = "execution(* com.knu.subway.webSocket.WebSocketHandler.sendData(..)) && args(session, message)", returning = "result")
+    @AfterReturning(pointcut = "execution(* com.knu.subway.webSocket.WebSocketHandler.sendData(..)) && args(session, message)", returning = "result", argNames = "session,message,result")
     public void logAfterSendingData(WebSocketSession session, String message, List<Subway> result) {
         for(Subway data : result) {
             System.out.println(data.getBtrainNo());
         }
     }
 
-    @AfterThrowing(pointcut = "execution(* com.knu.subway.webSocket.WebSocketHandler.sendData(..)) && args(session, message)", throwing = "error")
+    @AfterThrowing(pointcut = "execution(* com.knu.subway.webSocket.WebSocketHandler.sendData(..)) && args(session, message)", throwing = "error", argNames = "session,message,error")
     public void logAfterThrowingException(WebSocketSession session, String message, Throwable error) {
         log.error("Error while processing station data for message: {}", message, error);
     }
