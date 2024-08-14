e2368543a21ef7d46a811ef29503331289a3e3e9
JShistory <behappy0916@naver.com>
2024-08-14 17:56:36 +0900

    fix: 쿠키 초기화 시간 5분으로 변경

--- src/main/java/com/knu/subway/service/SubwayDataCollector.java
+++ src/main/java/com/knu/subway/service/SubwayDataCollector.java
@@ -53,7 +53,7 @@ public class SubwayDataCollector {
         }
     }
 
-    @Scheduled(cron = "0 0 5 * * *")
+    @Scheduled(fixedRate = 300000)
     public void subwayCookie() {
         log.info("delete Subway Cookie {} : ",subwayCookie);
         subwayCookie.clear();
